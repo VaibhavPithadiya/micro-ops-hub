1e7c46b1ca2a095020b055dd8d46715ce5fa8b14
VaibhavPithadiya <vaibhavpithadiya09@gmail.com>
2025-11-09 21:53:01 +0530

    jenkins file init changes

--- ci-cd/jenkins/scripts/build-images.sh
+++ ci-cd/jenkins/scripts/build-images.sh
@@ -2,8 +2,9 @@
 set -e
 
 echo "🔨 Building service images..."
-docker build -t localhost:5000/users-service:latest ./services/users-service
-docker build -t localhost:5000/orders-service:latest ./services/orders-service
-docker build -t localhost:5000/frontend:latest ./services/frontend
+pwd
+docker build -t localhost:5000/users-service:latest ./services
+docker build -t localhost:5000/orders-service:latest ./services
+docker build -t localhost:5000/frontend:latest ./services
 
 echo "✅ Build complete."
